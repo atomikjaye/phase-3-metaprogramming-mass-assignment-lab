2eab9ed358dfcdc9f3a02a4984cf07efa06fa787
Jocelyn Naarden <tzionone@gmail.com>
2022-07-20 16:43:49 -0400

    Finished Assignment ✔

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
+  def initialize(attrbutes)
+    # We wan to add getter and setters and send info to initialize
+    # we will iterate through attributes hash
+    attrbutes.each do |key, value| 
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
